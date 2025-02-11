9e303a4e090ddb1e3f898e7230a106fa9807a469
Aniket Jhariya <aniket.jhariya@gmail.com>
2025-02-11 15:55:15 +0530

    Adds separate login page.

---
+++ src/pages/login.js
@@ -0,0 +1,10 @@
+import React from "react";
+import EmailLoginForm from "@site/src/components/EmailLoginForm";
+
+export default function LoginPage() {
+    return (
+        <div>
+            <EmailLoginForm />
+        </div>
+    );
+}
